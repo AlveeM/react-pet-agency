58392838c650174adba71cb75dd41a4cdfe90547
Alvee Akand <alvee.akand@outlook.com>
2021-05-24 17:16:54 -0400

    lesson completed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import "./App.css";
+import PetContainer from "./components/PetContainer/PetContainer";
 
 function App() {
   return (
     <main className="app">
       <h1 className="app-heading">Pet Adoption Agency</h1>
+      <PetContainer />
     </main>
   );
 }
