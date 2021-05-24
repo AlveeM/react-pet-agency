94619c08a107abc5d8244a4292fc55d7a5584081
Alvee Akand <alvee.akand@outlook.com>
2021-05-24 15:00:59 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <main className="app">
+      <h1 className="app-heading">Pet Adoption Agency</h1>
+    </main>
+  );
+}
+
+export default App;
